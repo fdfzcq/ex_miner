1d99fc32575e3d8fbca47ddcbfc92848e6022657
sophyc <sophy.cao@derivco.se>
2018-07-06 18:28:55 +0200

    Add Plug and Cowboy

--- mix.exs
+++ mix.exs
@@ -15,13 +15,15 @@ defmodule ExMiner.Mixfile do
   def application do
     [
       mod: {ExMiner, []},
-      applications: [:explot]
+      applications: [:explot, :cowboy]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:cowboy, git: "https://github.com/ninenines/cowboy.git", tag: "2.1.0", override: true},
+      {:ranch, git: "https://github.com/ninenines/ranch", tag: "1.4.0", override: true},
       {:explot, "~> 0.1.0"}
     ]
   end
