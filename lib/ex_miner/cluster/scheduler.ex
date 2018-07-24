01f9b9345cafc8a0dd6bf4ac9d0a5707b75d7d31
sophyc <sophy.cao@derivco.se>
2018-07-24 17:32:47 +0200

    bug fix & make it possible to terminate scheduler

--- lib/ex_miner/cluster/scheduler.ex
+++ lib/ex_miner/cluster/scheduler.ex
@@ -5,15 +5,22 @@ defmodule ExMiner.Cluster.Scheduler do
 
   def init(_), do: {:ok, []}
 
-  def schedule(scheduler_name, repeat_args) do
-    pid = GenServer.call(:whereis)
-    Process.send_after(pid, {:repeat, repeat_args})
+  def schedule(scheduler_name, repeat_args = {mod, func, args, interval}) do
+    pid = GenServer.call(__MODULE__, :whereis)
+    Process.send_after(pid, {:repeat, repeat_args}, interval)
   end
+
+  def stop(), do: GenServer.stop(__MODULE__)
 
   def handle_info({:repeat, {mod, func, args, interval}}, state) do
     apply(mod, func, args)
-    Process.send_after(self(), {:repeat, {mod, func, args, interval}})
+    Process.send_after(self(), {:repeat, {mod, func, args, interval}}, interval)
     {:noreply, state}
+  end
+
+  def terminate(:normal, _state) do
+    IO.puts "scheduler stopped"
+    state
   end
 
   def handle_call(:whereis, _from, state), do: {:reply, self(), state}
